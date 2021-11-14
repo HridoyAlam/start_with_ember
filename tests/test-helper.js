5c5030e3f7c5f1f86bc608b11890a26a142f373a
HridoyAlam <jahidul.alam@northsouth.edu>
2021-11-14 22:33:09 +0600

    t-2

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'start-with-ember/app';
+import config from 'start-with-ember/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
